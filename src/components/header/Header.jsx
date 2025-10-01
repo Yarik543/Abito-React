d82774e45d24d3079a48301a12b98113638a64ae
Yarik543 <aroslavkasymbekov@gmail.com>
2025-10-01 21:58:17 +0300

    Готов 4 урок

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import './Header.css'
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="header-box">
+                    <a href="#!" className="header-logo">
+                        <img src="/image/logo.svg" alt="logo" />
+                        <span>Abito</span>
+                    </a>
+                    <div className="header-controls">
+                        <button className="btn btn-outline" type="button">
+                            Вход и регистрация
+                        </button>
+                        <button className="btn btn-primary" type="button">
+                            Подать обьявление
+                        </button>
+                    </div>
+                    <div className="header-burger">
+                        <img src="/image/burger.svg" alt="burger" />
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
